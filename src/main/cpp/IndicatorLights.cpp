5f7fc564bfb15ff2993d2ac43eab18ae354a2bcc
Michael Kaatz <michaelkaatz2012@hotmail.com>
2020-02-27 12:02:28 -0500

    Don't resend already executing commands

--- src/main/cpp/IndicatorLights.cpp
+++ src/main/cpp/IndicatorLights.cpp
@@ -37,28 +37,29 @@ IndicatorLights::~IndicatorLights() {
 }
 
 // Sets the arduino to execute a predefined command.
-void IndicatorLights::setCommand(CMD command) {
+// If resend if false, it will only send data if the arduino is not currently executing the same command.
+void IndicatorLights::setCommand(CMD command, bool resend) {
 	switch (command) {
 	case CMD::NONE:
-		sendCommand(CMD_INTERNAL::NOOP, 0, 0, 0);
+		sendCommand(CMD_INTERNAL::NOOP, 0, 0, 0, resend);
 		break;
 	case CMD::GREEN_SOLID:
-		sendCommand(CMD_INTERNAL::GREEN_SOLID, 0, 0, 0);
+		sendCommand(CMD_INTERNAL::GREEN_SOLID, 0, 0, 0, resend);
 		break;
 	case CMD::GREEN_UP:
-		sendCommand(CMD_INTERNAL::GREEN_UP, 0, 0, 0);
+		sendCommand(CMD_INTERNAL::GREEN_UP, 0, 0, 0, resend);
 		break;
 	case CMD::GREEN_DOWN:
-		sendCommand(CMD_INTERNAL::GREEN_DOWN, 0, 0, 0);
+		sendCommand(CMD_INTERNAL::GREEN_DOWN, 0, 0, 0, resend);
 		break;
 	case CMD::RED_SOLID:
-		sendCommand(CMD_INTERNAL::RED_SOLID, 0, 0, 0);
+		sendCommand(CMD_INTERNAL::RED_SOLID, 0, 0, 0, resend);
 		break;
 	case CMD::RED_UP:
-		sendCommand(CMD_INTERNAL::RED_UP, 0, 0, 0);
+		sendCommand(CMD_INTERNAL::RED_UP, 0, 0, 0, resend);
 		break;
 	case CMD::RED_DOWN:
-		sendCommand(CMD_INTERNAL::RED_DOWN, 0, 0, 0);
+		sendCommand(CMD_INTERNAL::RED_DOWN, 0, 0, 0, resend);
 		break;
 	default:
 		// Invalid command?
@@ -69,17 +70,28 @@ void IndicatorLights::setCommand(CMD command) {
 // Sets the LEDs to a custom RGB color.
 // Only the lower 8 bits are used.
 // Basically only use numbers 0-255.
-void IndicatorLights::setColor(int r, int g, int b) {
-	sendCommand(CMD_INTERNAL::CUSTOM, r, g, b);
+// If resend if false, it will only send data if the arduino is not currently set to the same color.
+void IndicatorLights::setColor(int r, int g, int b, bool resend) {
+	sendCommand(CMD_INTERNAL::CUSTOM, r, g, b, resend);
 }
 
 // Send command to the arduino for processing.
-void IndicatorLights::sendCommand(CMD_INTERNAL command, int r, int b, int g) {
-	// Create array to house the opcode and each operand.
-	// We cast to char as SerialPort::Write() takes a char pointer.
-	const char buffer[4] = { (char) ((int) command), (char) r, (char) g, (char) b };
-	// Send the bytes to the function.
-	serial->Write(buffer, 4);
-	// Flush to ensure nothing lingered. This should do nothing but I like to be sure.
-	serial->Flush();
+void IndicatorLights::sendCommand(CMD_INTERNAL command, int r, int g, int b, bool resend) {
+	// Check if executing command is same as sending command.
+	// If so, and resend is false, don't send new command.
+	if (resend || (current != command) || (operands[0] != r) || (operands[1] != g) || (operands[2] != b)) {
+		// Create array to house the opcode and each operand.
+		// We cast to char as SerialPort::Write() takes a char pointer.
+		const char buffer[4] = { (char) ((int) command), (char) r, (char) g, (char) b };
+		// Send the bytes to the function.
+		serial->Write(buffer, 4);
+		// Flush to ensure nothing lingered. This should do nothing but I like to be sure.
+		serial->Flush();
+		
+		// Update current command and operands
+		current = command;
+		operands[0] = r;
+		operands[1] = g;
+		operands[2] = b;
+	}
 }
